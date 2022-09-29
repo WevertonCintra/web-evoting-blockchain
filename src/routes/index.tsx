830b3a95d8ef7d095194be197e6dc0389a5fb39e
Caio <caio.vsg@hotmail.com>
2022-09-29 19:35:08 -0300

    Bug dos exports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Evento from'../pages/Evento'
 import Login from'../pages/Login'
+import RegisterVoting from '../pages/RegisterVoting'
 
 export function Router() {
   return (
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/evento" element={<Evento />} />
+      <Route path="/registerVoting" element={<RegisterVoting />} />
     </Routes>
   )
 }
