6b617f879e07642d7bb5e3b49d9f3c6e59ef16ce
Caio <caio.vsg@hotmail.com>
2022-09-30 00:42:08 -0300

    ListVotes com problema no topbar

---
+++ src/pages/ListVotes.tsx
@@ -0,0 +1,61 @@
+import { TableVoting, TableTd, Title, TopBar, TableTh, TableTr, TableLink, BtnVoltar, ContentContainer, Btn } from '../styles/ListVotes'
+
+import View from '../assets/view.png'
+import { TopGrid } from '../styles/RegisterVoting'
+import { theme } from '../styles/theme'
+
+
+export default function ListVotes() {
+  return (
+    <ContentContainer >
+        <TopGrid>
+            <TopBar>
+                <Title>Minhas Votações</Title>
+            
+             </TopBar>
+             <Btn color={theme['blue']}>Adicionar</Btn>
+
+        </TopGrid>
+      
+
+      <TableVoting>
+        <tbody>
+          <TableTr>
+            <TableTh>
+              Título
+            </TableTh>
+            <TableTh>
+              Descrição
+            </TableTh>
+            <TableTh>
+                Status
+            </TableTh>
+            <TableTh>
+              Fim
+            </TableTh>
+            <TableTh>
+              Ações
+            </TableTh>
+          </TableTr>
+
+          <TableTr>
+            <TableTd>Primeira Votação</TableTd>
+            <TableTd>Votação da votação</TableTd>
+            <TableTd>Finalizado</TableTd>
+            <TableTd>29/10/2022 12:00</TableTd>
+            <TableTd><TableLink to="/ViewVoting"><img src={View} width="26"></img></TableLink></TableTd>
+          </TableTr>
+          <TableTr>
+            <TableTd>Segunda Votação</TableTd>
+            <TableTd>Votação da votação da votação</TableTd>
+            <TableTd>Em Andamento</TableTd>
+            <TableTd>29/10/2022 12:30</TableTd>
+            <TableTd><TableLink to="/ViewVoting"><img src={View} width="26"></img></TableLink></TableTd>
+          </TableTr>
+        </tbody>
+      </TableVoting>
+
+      <BtnVoltar>Voltar</BtnVoltar>
+    </ContentContainer>
+  )
+}
