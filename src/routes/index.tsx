c8c6a824f12b6938ab76c0088eb6f7d09766a5b8
WevertonCintra <wevertoncintra@outlook.com>
2022-09-29 22:55:29 -0300

    adding struct generate file key

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
+import { SignUp } from '../pages/SignUp'
+import { SignIn } from '../pages/SignIn'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/registro" element={<SignUp />} />
+      <Route path="/acesso" element={<SignIn />} />
     </Routes>
   )
 }
--- src/utils/ECDSA/exportKey.ts
+++ src/utils/ECDSA/exportKey.ts
@@ -1,12 +1,11 @@
-function exportKey(publicKey: any) {
-  window.crypto.subtle
+async function exportKey(publicKey: any) {
+  return window.crypto.subtle
     .exportKey(
       'jwk', // can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
       publicKey, // can be a publicKey or privateKey, as long as extractable was true
     )
     .then(function (keydata) {
-      // returns the exported key data
-      console.log(keydata)
+      return keydata
     })
     .catch(function (err) {
       console.error(err)
