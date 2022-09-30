201b762a57ac4946b2fb2251009569a34f5f058f
Edgarvital <edgar.tecnico@hotmail.com>
2022-09-29 22:11:29 -0300

    Correção no css da página de votação

--- src/styles/Vote.ts
+++ src/styles/Vote.ts
@@ -6,41 +6,6 @@ export const ContentContainer = styled.div`
   margin-top: 5%;
 `;
 
-export const TableVoting = styled.table`
-  border-collapse: collapse;
-  width: 100%;
-`;
-
-export const TableTd = styled.td`
-  padding: 8px;
-  text-align: center;
-  border-bottom: 1px solid #ddd;
-`;
-
-export const TableTh = styled.th`
-  padding: 8px;
-  text-align: center;
-  border-bottom: 1px solid #ddd;
-`;
-
-export const TableTr = styled.tr`
-   &:not(:first-child):hover{
-    background-color: #E1E1E6;
-   };
-`;
-
-export const TableLink = styled(Link)`
-  color: white;
-  font-size: medium;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: none;
-  margin: 10px;
-  margin-top: 25px;
-  @media (max-width: 700px) {
-    display: none;
-  }
-`;
-
 export const Btn = styled.button`
   background-color: ${props => `${props.color}`};
   border: 1px solid ${props => `${props.color}`};
